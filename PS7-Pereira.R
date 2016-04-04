c62a8f55dfbd363c968b8c8a3d193ee4097c58f8
miguelmariagp <m.pereira@wustl.edu>
2016-04-04 15:47:56 -0500

    Last exercise
    First take on MC integration

--- PS7-Pereira.R
+++ PS7-Pereira.R
@@ -41,17 +41,6 @@ sg.int<-function(g,...,lower,upper)
 }
 
 
-ll
-uu
-
-cbind(ll,uu)
-
-gg<-as.matrix(expand.grid(apply(cbind(ll,uu),
-                            1,function(x) seq(x[1],x[2]-1,by=1))))
-
-gg[1,]
-as.matrix(expand.grid(seq(ll[1],uu[1]-1,by=1),
-                      seq(ll[2],uu[2]-1,by=1)))
 
 
 ###########
@@ -66,7 +55,7 @@ sg.int.multidim<-function(f,lower,upper, parallel=FALSE){
   #The necessary instructions for the parallel option to work
   if (parallel==TRUE){
     require("doParallel")
-    registerDoParallel(,cores=2)
+    registerDoParallel(cores=4)
     #registerDoSNOW(makeCluster(2, type = "SOCK"))
   }
 
@@ -149,6 +138,7 @@ sg.int.multidim(f,lower=lll,upper=uuu)
 #Exercise 3
 ###########
 
+#####TO DO
 
 library(testthat)
 test_that("Correct inputs",{
@@ -159,6 +149,28 @@ test_that("Correct inputs",{
 })
 typeof(fun)
 
+
+test_that("Basic output test", {
+  ll<-c(1,2,3)
+  uu<-c(4,6,8)
+  f<-function(x) x^2
+  int <- sg.int.multidim(f,lower=ll,upper=uu)
+  
+  expect_that( int, is_a("matrix") )
+  expect_that(length(int), equals(length(ll)))
+})
+
+
+
+test_that("Output test", {
+  ll<-c(1,2,3)
+  uu<-c(4,6,8)
+  f<-function(x) x^2
+  int <- sg.int.multidim(f,lower=ll,upper=uu)
+  
+  expect_that( int, is_a("matrix") )
+  expect_that(length(int), equals(length(ll)))
+})
 
 
 
@@ -197,7 +209,57 @@ example1 <- function(x){
 ans <- as.numeric(pmvnorm(upper=rep(1, 2), mean=rep(0, 2), sigma=diag(rep(1, 2))))
 
 #With adaptIntegrate the result is very close to the truth
-adaptIntegrate(myNorm, lowerLimit=rep(-100, 2), upperLimit=rep(1, 2))$integral - ans
+adaptIntegrate(example1, lowerLimit=rep(-100, 2), upperLimit=rep(1, 2))$integral - ans
 #With my function it is not so close:
-sg.int.multidim(myNorm, lower=rep(-1, 2), upper=rep(1, 2)) - ans
+sg.int.multidim(example1, lower=rep(-1, 2), upper=rep(1, 2)) - ans
 
+
+
+###########
+#Exercise 6
+###########
+
+mc.int<-function(f,lower,upper, parallel=FALSE){ 
+  require("SparseGrid")
+  require("plyr")
+  require("mvtnorm")
+  #The necessary instructions for the parallel option to work
+  if (parallel==TRUE){
+    require("doParallel")
+    registerDoParallel(cores=4)
+  }
+  
+  lower<-floor(lower)
+  upper<-ceiling(upper)
+  
+  if (any(lower>upper)) stop("lower must be smaller than upper")
+  
+  #Number of dimensions derived from the length of the vectors introduced
+  dim<-length(lower)
+  
+  #This gives us all possible combinations of the supplied vectors
+  gridss<-as.matrix(expand.grid(apply(cbind(lower,upper),
+                                      1,function(x) seq(x[1],x[2]-1,by=1))))
+  
+  #Note: dimension = number of dimensions wanted
+  sp.grid <- createMonteCarloGrid(rnorm, dimension=2, num.sim=100 )
+  nodes<-gridss[1,]+sp.grid$nodes
+  weights<-sp.grid$weights
+  
+  for (i in 2:nrow(gridss)){
+    nodes<-rbind(nodes,gridss[i,]+sp.grid$nodes)  
+    weights<-c(weights,sp.grid$weights)
+  }
+  
+  gx.sp <- aaply(.data=nodes,
+                 .margins = 1,
+                 .parallel = parallel,
+                 .fun = f)
+  val.sp <- t(gx.sp) %*%weights
+  return(val.sp)
+  
+}
+
+
+sg.int.multidim(example1, lower=rep(-1, 2), upper=rep(1, 2))
+mc.int(example1, lower=rep(-1, 2), upper=rep(1, 2))
